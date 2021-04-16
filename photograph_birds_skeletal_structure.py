6a927d19e50ab372d6005e15bbff7899c259574d
farhanquader <farhan.quader@technuf.com>
2021-04-16 15:26:39 -0400

    Add files via upload

---
+++ photograph_birds_skeletal_structure.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+class Coordinates:
+    def __init__(self):
+
+    def grid():
+
+    def user():
+
+    def user_birds():
+
+
+
+
+
+class Collection_of_Birds:
+    def __init__(self):
+
+    def bird_list():
+
+
+def main():
+    print()
